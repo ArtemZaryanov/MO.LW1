585e7badabf04fe7b3a9d6e8a47a9db47ace17ec
ArtemZaryanov <artemzaryanov2013@yandex.ru>
2020-10-04 13:18:48 +0300

    Закончен метод золотого сечения

--- MO.LW1/MO.LW1.cpp
+++ MO.LW1/MO.LW1.cpp
@@ -2,22 +2,54 @@
 //
 
 #include <iostream>
-#include <math.h>
 #include <functional>
+
+double Function(const double x);
+void Sven(double& a, double& b, const double t);
+double Dichotomy(const double a, const double b, const double e);
+double GoldSection(const double a, const double b, const double e);
+double Fibonacci(const double a, const double b, const double e);
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    double a = 0.5;
+    double b = 10.5;
+    const double t = (b - a) / 100;
+    
+    std::cout << "Sven..." << std::endl;
+    Sven(a, b, t);
+    std::cout << "Search interval : " << a << ", " << b << std::endl;
+    std::cout << "Dichotomy" << std::endl;
+    const double e = 0.0001;
+    double x = Dichotomy(a, b, e);
+    std::cout << "Dichotomy::Minimum x=" << x << " f(x)=" << Function(x) << std::endl;
+    std::cout << "GoldSection" << std::endl;;
+    x = GoldSection(a, b, e);
+    std::cout << "GoldSection::Minimum x=" << x << " f(x)=" << Function(x) << std::endl;
+    //TODO Проверить функию. По идее она должна быть быстрее в смысле числа итераций
+}
 
 double Function(const double x)
 {
-    return (x - 2) * (x - 2);
+    if (x < 1)
+    {
+        return 1 / (1 + x * x);
+    }
+    else
+    {
+        return (2 * x * x - 5 * x - 15) / (x * x * x * x);
+    }
 }
 void Sven(double& a, double& b, const double t)
 {
     double x0 = (a + b) / 2;
     double f0 = Function(x0 - t);
-    double f = Function(x0 - t);
+    double f = Function(x0);
     double f1 = Function(x0 + t);
     if (!((f >= f0) && (f >= f1)))
     {
-         //Функция унимодальна!
+        //Функция унимодальна!
         if ((f <= f0) && (f <= f1))
         {
             a = x0 - t;
@@ -41,19 +73,22 @@ void Sven(double& a, double& b, const double t)
 
         }
         //Собственно сам поиск
-        int buf = 0;
+        double buf = 0;
         int k = 1;
+        std::cout << "delta=" << delta << std::endl;
+        std::cout << "Search..." << std::endl;
         while (true)
         {
-            buf = x0 + pow(2, k) * delta;
+            buf = x0 +2*k * delta;
             x0 = x1;
             x1 = buf;
             if (Function(x1) < Function(x0))
             {
                 //Могут быть ошибки точности ы
-                if (delta == t)
+                if (delta>0)
                 {
                     a = x0;
+                    //std::cout << "+" << (delta - t)<< std::endl;
                 }
                 else
                 {
@@ -65,7 +100,7 @@ void Sven(double& a, double& b, const double t)
             else
             {
                 //Могут быть ошибки точности ы
-                if (delta == t)
+                if ((delta > 0))
                 {
                     b = x1;
                 }
@@ -73,49 +108,105 @@ void Sven(double& a, double& b, const double t)
                 {
                     a = x1;
                 }
+                return;
             }
             k = k + 1;
-            
+
 
         }
     }
     else
     {
-        //функция не унимодальна 
+        //функция не унимодальна
     }
 }
-void Dichotomy()
+double Dichotomy(const double a , const double b, const double e)
+{
+    double iter = 1;
+    double ak = a;
+    double bk = b;
+    double ck = (a + b) / 2;
+    do
+    {
+        //std::cout << "Search interval : " << ak << ", " << bk << std::endl;
+        double fck = Function(ck);
+        double yk = (ak + ck) / 2;
+        double fy = Function(yk);
+        double zk = 0;
+        double fzk = 0;
+        if (fy <= fck)
+        {
+            ak = ak;
+            bk = ck;
+            ck = yk;
+        }
+        if (fy > fck)
+        {
+            zk = (bk + ck) / 2;
+            fzk = Function(zk);
+            if (fck <= fzk)
+            {
+                ak = yk;
+                bk = zk;
+                ck = ck;
+            }
+            else
+            {
+                ak = ck;
+                bk = bk;
+                ck = zk;
+            }
+        }
+        
+        iter++;
+    } while (abs(bk - ak)>=e);
+    std::cout << "iter=" << iter << std::endl;
+    return (ak + bk) / 2;
+}
+double GoldSection(const double a, const double b, const double e)
+{
+    std::function<double(double, double)> ykgs = [](double a, double b)
+    {
+        return a + ((3 - sqrt(5)) / 2) * (b - a);
+    };
+    std::function<double(double, double)> zkgs = [](double a, double b)
+    {
+        return a + ((sqrt(5) -1) / 2) * (b - a);
+    };
+    double iter = 1;
+    double ak = a;
+    double bk = b;
+    //double ck = (a + b) / 2;
+    do
+    {
+        //std::cout << "Search interval : " << ak << ", " << bk << std::endl;
+        //double fck = Function(ck);
+        double yk = ykgs(ak,bk);
+        double fyk = Function(yk);
+        double zk = zkgs(ak, bk);
+        double fzk = Function(zk);
+        if (fyk <= fzk)
+        {
+            ak = ak;
+            bk = zk;
+            yk = ykgs(ak, bk);
+            zk = yk;
+        }
+        else
+        {
+            ak = yk;
+            bk = bk;
+            yk = zk;
+            zk = zkgs(ak, bk);
+        }
+
+        iter++;
+    } while (abs(bk - ak) >= e);
+    std::cout << "iter=" << iter << std::endl;
+    return (ak + bk) / 2;
+
+}
+double Fibonacci(const double a, const double b, const double e)
 {
     ;
 }
-void GoldSection()
-{
-    ;
-}
-void Fibonacci()
-{
-    ;
-}
-int main()
-{
-    std::cout << "Hello World!\n";
-    double a = 0;
-    double b = 10;
-    const double t = (b - a) / 100;
-    Sven(a, b, t);
-    std::cout << "Sven..." << std::endl;
-    std::cout << "Search interval : " << a << ", " << b << std::endl;
-    
-
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
